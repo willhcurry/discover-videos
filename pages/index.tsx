4535a322b1f3f68699de5d3ac6a24c052aa8a971
willhcurry <willhcurry@gmail.com>
2024-08-24 17:56:17 -0600

    Initial card component with error handling

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import styles from "@/styles/Home.module.css";
 
 import Banner from '../components/banner/banner';
 import NavBar from '../components/nav/navbar';
+import Card from '../components/card/card';
 
 
 export default function Home() {
@@ -20,9 +21,10 @@ export default function Home() {
         subTitle='A childrens story'
         imgUrl='/static/clifford.webp' />
 
-      {/* {<NavBar />
       
-      <Card />} */}
+      <Card imgUrl='/static/clifford.webp' size='large' />
+      <Card imgUrl='/static/clifford.webp' size='medium' />
+      <Card imgUrl='/static/clifford.webp' size='small' />
 
     </div>
   );
