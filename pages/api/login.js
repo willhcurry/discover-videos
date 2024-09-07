8a2e0c1668e7ffc29d31778cae14d1dcd1018691
willhcurry <willhcurry@gmail.com>
2024-09-06 20:07:51 -0600

    implemented server to receive magic auth token

---
+++ pages/api/login.js
@@ -0,0 +1,21 @@
+import { magicAdmin } from '../../lib/magic';
+
+export default async function login(req, res) {
+    if(req.method === "POST") {
+        try {
+            const auth = req.headers.authorization;
+            const didToken = auth ? auth.substr(7) : "";
+            console.log({didToken});
+
+            const metadata = await magicAdmin.users.getMetadataByToken(didToken);
+            console.log({ metadata });
+
+            res.send({ done: true });
+        } catch(error) {
+            console.error('Something went wrong logging in', error);
+            res.status(500).send({ done: false });
+        }
+    } else {
+        res.send({ done: false });
+    }
+}
